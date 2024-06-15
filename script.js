e7889ea9eaf8cb115b0e34e63e44bf50863154b2
Nebular6 <104096469+Nebular6@users.noreply.github.com>
2024-06-15 01:03:52 +0100

    added stying and cool JS effect, also fixed link

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const underscore = document.getElementById('underscore'); // every element tagged "underscore" gets it applied,
+    let showUnderscore = true;                                // however has to have contrainers used cuz i wanted it centered
+
+    setInterval(() => {
+        if (showUnderscore) {       //if its true(showing)
+            underscore.textContent = "_";//it shows
+        } else {//if its not
+            underscore.textContent = "";//it deletes it
+        }
+        showUnderscore = !showUnderscore; //easiest and coolest way to swap true and false every iteration
+    }, 500); //ms interval time
+});
